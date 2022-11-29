f099006915733d9ce45e4ce6940de26745d27ed8
RodriguesCovre <112516755+RodriguesCovre@users.noreply.github.com>
2022-11-29 20:03:20 -0300

    Add files via upload

---
+++ js/models.js
@@ -0,0 +1,10 @@
+let modelsJson = [
+    {id:1, name:'Carne de Soja', img:'img/image 74.png', price:[19.00, 26.00, 39.00], sizes:['1/72', '1/48', '1/32'], description:'Hambúrguer de carne de  soja (150g) recheado com queijo, queijo chedar, tomate, cebola, alface e molho especial.'},
+    {id:2, name:'Pizza de Camarão', img:'img/image 114.png', price:[35.00, 50.00, 110.00], sizes:['1/72', '1/48', '1/32'], description:'Molho, muçarela, camarões ao molho, catupiry e orégano.'},
+    {id:3, name:'Pizza de chocolate Preto', img:'img/image 105.png', price:[35.00, 50.00, 110.00], sizes:['1/72', '1/48', '1/32'], description:'Muçarela, chocolate ao leite.'},
+    {id:4, name:'Frango Duplo', img:'img/image 69.png', price:[29.90, 35.00, 50.00], sizes:['1/72', '1/48', '1/32'], description:'Hambúrguer de frango duplo , queijo chedar,  alface e molho especial.'},
+    {id:5, name:'Burger de Siri', img:'img/image 39 (1).png', price:[35.00, 50.00, 110.00], sizes:['1/72', '1/48', '1/32'], description:'Hambúrguer de carne de  costela (150g), queijo chedar, tomate, cebola, picles, alface e molho especial.'},
+    {id:6, name:'Porção de Batata', img:'img/dish_thumbnail.webp', price:[7.90, 30.00, 49.90], sizes:['1/72', '1/48', '1/32'], description:'Batata'},
+    {id:7, name:'Porção de Nugget', img:'img/image 79.png', price:[7.90, 30.00, 49.90], sizes:['1/72', '1/48', '1/32'], description:'Mini frango empanado'},
+    {id:8, name:'Bolo de Chocolate', img:'img/image 121.png', price:[15.90, 20.90, 60.00], sizes:['1/72', '1/48', '1/32'], description:'Bolo de chocolate com recheio de brigadeiro.'}
+];
